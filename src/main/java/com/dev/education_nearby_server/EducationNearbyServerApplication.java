04d0d50c31085978410868f0495d489e4f0d36cf
TodorYanakiev <todoryanakiev395@gmail.com>
2025-10-01 11:11:07 +0300

    Initial commit

---
+++ src/main/java/com/dev/education_nearby_server/EducationNearbyServerApplication.java
@@ -0,0 +1,13 @@
+package com.dev.education_nearby_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationNearbyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EducationNearbyServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/education_nearby_server/EducationNearbyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.education_nearby_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EducationNearbyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
