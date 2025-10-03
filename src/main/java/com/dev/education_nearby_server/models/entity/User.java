ca3f42eaf5f1455dea196e2c659433b0afbd1a6e
TodorYanakiev <todoryanakiev395@gmail.com>
2025-10-03 11:06:37 +0300

    Add User entity

---
+++ src/main/java/com/dev/education_nearby_server/models/entity/User.java
@@ -0,0 +1,86 @@
+package com.dev.education_nearby_server.models.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name="_users")
+public class User implements UserDetails {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long id;
+
+    @NotNull(message = "The firstname should not be null!")
+    private String firstname;
+
+    @NotNull(message = "The lastname should not be null!")
+    private String lastname;
+
+    @Email(message = "Invalid email!")
+    @NotNull(message = "The email should not be null!")
+    @Column(unique = true)
+    private String email;
+
+    @NotNull(message = "The password should not be null!")
+    @Length(min = 8, message = "The length should be at least 8!")
+    private String password;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    private boolean enabled;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime deletedAt;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+}
