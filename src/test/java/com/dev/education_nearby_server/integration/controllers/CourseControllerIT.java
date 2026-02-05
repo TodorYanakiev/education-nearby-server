a2695f1c692887ab71cdcc1cb6c16d0ea3502c23
TodorYanakiev <todoryanakiev395@gmail.com>
2026-02-06 00:05:44 +0200

    Adjust test to match new logic

--- src/test/java/com/dev/education_nearby_server/integration/controllers/CourseControllerIT.java
+++ src/test/java/com/dev/education_nearby_server/integration/controllers/CourseControllerIT.java
@@ -88,6 +88,7 @@ class CourseControllerIT {
                         .param("maxPrice", "45.5")
                         .param("recurrence", ScheduleRecurrence.WEEKLY.name())
                         .param("dayOfWeek", DayOfWeek.MONDAY.name())
+                        .param("dayOfWeek", DayOfWeek.WEDNESDAY.name())
                         .param("startTimeFrom", "09:00")
                         .param("startTimeTo", "18:00")
                         .param("activeStartMonth", Month.MAY.name())
@@ -106,7 +107,7 @@ class CourseControllerIT {
         assertThat(captured.getMinPrice()).isEqualTo(15f);
         assertThat(captured.getMaxPrice()).isEqualTo(45.5f);
         assertThat(captured.getRecurrence()).isEqualTo(ScheduleRecurrence.WEEKLY);
-        assertThat(captured.getDayOfWeek()).isEqualTo(DayOfWeek.MONDAY);
+        assertThat(captured.getDayOfWeek()).containsExactly(DayOfWeek.MONDAY, DayOfWeek.WEDNESDAY);
         assertThat(captured.getStartTimeFrom()).isEqualTo(LocalTime.of(9, 0));
         assertThat(captured.getStartTimeTo()).isEqualTo(LocalTime.of(18, 0));
         assertThat(captured.getActiveStartMonth()).isEqualTo(Month.MAY);
